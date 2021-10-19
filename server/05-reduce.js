5791b80bee5ed084b58c07456b6045377a14b35e
Christiano0407 <placebo0704@gmail.com>
2021-10-19 09:15:11 -0500

    reduceReloaded

--- server/05-reduce.js
+++ server/05-reduce.js
@@ -1,3 +1,34 @@
 console.group("Method Reduce");
 
 console.groupEnd();
+console.group("Reduce");
+// < Method REDUCE ==>Lo reduce a un sólo valor>> Cumplir con la condición>>
+
+const totals = [1, 2, 3, 4, 5];
+
+// Ciclo For >>>>>>>>>>>>
+let sum = 0; // << => INDEX O ITEM === POSICIÓN QUE ES 0 = 1 POSICIÓN>>
+for (let i = 0; i < totals.length; i++) {
+  const element = totals[i];
+  sum = sum + element;
+}
+console.log(sum);
+// <<<<<<<<<< Method Reduce >>>>>>>>>>
+//(A)>
+// El primer valor es el acumulador => Sum>>
+// 1> Arrow Function / 2> Estado Inicial (0); =====> Mandamos dos parámetros
+// (element(index) === item)>>
+//========================= (1) ========================= <(2)>
+const redu = totals.reduce((sum, element) => sum + element, 0);
+console.log(redu);
+
+// << EXERCISE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>=================================================>
+//(1)
+const value = [5, 10, 15, 20, 25, 30];
+//(2)
+const count = 5;
+//(3)
+const newValue = value.reduce((count, item) => count + item, 5);
+console.log(newValue);
+
+console.groupEnd();
---
+++ server/06-reduceReloaded.js
@@ -0,0 +1,3 @@
+console.group("ReduceReloaded");
+
+console.groupEnd();

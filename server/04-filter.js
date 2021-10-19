03c6400e2dc581e7ceec2f4ba511e50d939c88c3
Christiano0407 <placebo0704@gmail.com>
2021-10-19 08:08:14 -0500

    reduce

--- server/04-filter.js
+++ server/04-filter.js
@@ -17,4 +17,61 @@ console.log(newWord);
 const rta = words.filter((item) => item.length >= 6);
 console.log(rta);
 
+// =================================================================>>>>>>
+const orders = [
+  {
+    customerName: "Diana",
+    total: 60,
+    delivered: true,
+  },
+  {
+    customerName: "Luisa",
+    total: 90,
+    delivered: false,
+  },
+  {
+    customerName: "Mau",
+    total: 100,
+    delivered: false,
+  },
+  {
+    customerName: "Pammela",
+    total: 150,
+    delivered: true,
+  },
+  {
+    customerName: "Chris",
+    total: 200,
+    delivered: true,
+  },
+  {
+    customerName: "Karen",
+    total: 500,
+    delivered: false,
+  },
+  {
+    customerName: "Paola",
+    total: 100,
+    delivered: true,
+  },
+];
+console.log(orders);
+// Filter >>
+//(1)
+const rtaTwo = orders.filter((item) => item.delivered);
+console.log(rtaTwo);
+//(2)
+const rtaThree = orders.filter(
+  (item) => item.customerName === "Mau" || item.total >= 100
+);
+console.log(rtaThree);
+//(3) // Function and method>> Buscador>>Includes=> Incluir.(String)>>>
+const search = (searching) => {
+  return orders.filter((item) => {
+    return item.customerName.includes(searching);
+  });
+};
+console.log(search("Pa"));
+console.log(search("L"));
+
 console.groupEnd();
---
+++ server/05-reduce.js
@@ -0,0 +1,3 @@
+console.group("Method Reduce");
+
+console.groupEnd();

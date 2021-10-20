e413a4c109b6cd735c2ff7ecfc528b4fc4b8c255
Christiano0407 <placebo0704@gmail.com>
2021-10-20 09:12:13 -0500

    npm audit

--- server/someExercise.js
+++ server/someExercise.js
@@ -1,4 +1,7 @@
 // Exercise Two>
+// <== npm i date-fns ===>
+//import { areIntervalsOverlapping, isOverlap } from "date-fns";
+// (A)
 const dates = [
   {
     startDate: new Date(2021, 1, 1, 10),
@@ -16,9 +19,29 @@ const dates = [
     title: "Cena",
   },
 ];
-
+console.log(dates);
+//(B)
 const newAppointment = {
   startDate: new Date(2021, 1, 1, 8),
   endDate: new Date(2021, 1, 1, 9, 30),
 };
-console.log(dates);
+
+// (C) date-fns> npm>
+//const areIntervalsOverlapping = require("date-fns/areIntervalsOverlapping");
+const areIntervalsOverlapping = require(`date-fns/areIntervalsOverlapping`);
+//import format from "date-fns/format";
+//import { format } from "date-fns";
+/*areIntervalsOverlapping(intervalLeft, intervalRight, [options]);*/
+//const { areIntervalsOverlapping } = require("date-fns");
+
+// (F) Para saber si se sobreponen => date-fns>>>
+const isOverlap = (newDate) => {
+  return dates.some((date) => {
+    return areIntervalsOverlapping(
+      { start: date.startDate, end: date.endDate },
+      { start: newDate.startDate, end: newDate.endDate }
+    );
+  });
+};
+//(G)>
+console.log(`isOverlap`, isOverlap(newAppointment));

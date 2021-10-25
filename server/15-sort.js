e3200b12deda1c4673aea0b60d857c2787f285d6
Christiano0407 <placebo0704@gmail.com>
2021-10-25 08:09:20 -0500

    javascript Sort

---
+++ server/15-sort.js
@@ -0,0 +1,30 @@
+// Es un método mutable => Cambia el Array original.
+//>>
+const months = ["March", "January", "February", "December"];
+const numbers = [5, 50, 100, 150, 200, 500];
+const words = ["Premier", "Thor", "Iron Man", "Captain", "Eternals"];
+
+//>>
+const orders = [
+  {
+    customerName: "Nicolas",
+    total: 600,
+    delivered: true,
+  },
+  {
+    customerName: "Zulema",
+    total: 120,
+    delivered: false,
+  },
+  {
+    customerName: "Santiago",
+    total: 1840,
+    delivered: true,
+  },
+  {
+    customerName: "Valentina",
+    total: 240,
+    delivered: true,
+  },
+];
+console.log(orders);

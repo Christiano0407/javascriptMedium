9c03ca24cb103197a468adb91e5f040d0f16ca67
Christiano0407 <placebo0704@gmail.com>
2021-10-26 11:48:59 -0500

    async and Await finally

--- asyncronismo/retoAsyncAwait02.js
+++ asyncronismo/retoAsyncAwait02.js
@@ -1,3 +1,26 @@
 console.group("Practice Async and Await");
+//>>> (A) ==>
+const fetchData = require(`../asyncronismo/utils/fetchDataJs`);
+
+//>>> (b) ==>
+const API = `https://rickandmortyapi.com/api/character/`;
+
+//>>> (C) ==> Async and Await>>
+const dataAll = async (url_api) => {
+  try {
+    const data = await fetchData(url_api);
+    const character = await fetchData(`${API}${data.results[0].id}`);
+    const origin = await fetchData(character.origin.url);
+    //>
+    console.log(data.info.count);
+    console.log(data.name);
+    console.log(origin.dimension);
+    //>
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+console.log(dataAll(API));
 
 console.groupEnd();

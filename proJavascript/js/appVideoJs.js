103157b33b0b8bc144df0607983e1030f436804b
Christiano0407 <placebo0704@gmail.com>
2021-10-27 12:33:23 -0500

    video

---
+++ proJavascript/js/appVideoJs.js
@@ -0,0 +1,9 @@
+console.group("Platzi Video Media");
+// Const>>>
+// <<== Video ==>>
+const video = document.getElementById(`movie`);
+console.log(movie);
+const playBtn = document.getElementById(`playButton`);
+const muteBtn = document.getElementById(`muteButton`);
+
+// <<== Video Part==>>

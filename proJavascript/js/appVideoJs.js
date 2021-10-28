45663ebb090517bfdb4f6022782c25d8ad3fd64f
Christiano0407 <placebo0704@gmail.com>
2021-10-28 10:29:48 -0500

    progressBar

--- proJavascript/js/appVideoJs.js
+++ proJavascript/js/appVideoJs.js
@@ -5,8 +5,14 @@ export const video = document.getElementById(`movie`);
 console.log(movie);
 export const playBtn = document.getElementById(`playButton`);
 export const muteBtn = document.getElementById(`muteButton`);
-
+export const stopBtn = document.getElementById(`stopBtn`);
+export const progressBar = document.querySelector(".progress");
+export const timestamp = document.querySelector(".timestamp");
 // <<< ==== Importamos y Ejecutamos ==== >>>
 import { mediaPlayer } from "./videoOne.js";
 
 import { mediaMuted } from "./videoOne.js";
+
+import { stopVideo } from "./videoOne.js";
+
+import { setVideoProgress } from "./videoOne.js";
--- proJavascript/js/videoOne.js
+++ proJavascript/js/videoOne.js
@@ -2,10 +2,15 @@
 import { video } from "../js/appVideoJs.js";
 import { playBtn } from "../js/appVideoJs.js";
 import { muteBtn } from "../js/appVideoJs.js";
+import { stopBtn } from "../js/appVideoJs.js";
+import { progressBar } from "../js/appVideoJs.js";
+import { timestamp } from "../js/appVideoJs.js";
 //> <<== Video Part Events==>>
 video.addEventListener("click", mediaPlayer);
 playBtn.addEventListener("click", mediaPlayer);
 muteBtn.addEventListener("click", mediaMuted);
+stopBtn.addEventListener("click", stopVideo);
+progressBar.addEventListener("change", setVideoProgress);
 
 //> <<== EXPORTAR == Invocar====>
 export function mediaPlayer() {
@@ -20,4 +25,12 @@ export function mediaPlayer() {
 
 export function mediaMuted() {
   video.muted = true;
+}
+
+export function stopVideo() {
+  video.pause();
+}
+
+export function setVideoProgress() {
+  progressBar.value = Number((progressBar.value * video.duration) / 100);
 }

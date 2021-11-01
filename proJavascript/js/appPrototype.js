3850e5470f0ae29033a856eb4105a64e8c284523
Christiano0407 <placebo0704@gmail.com>
2021-11-01 09:06:38 -0600

    methods and prototypes and object

--- proJavascript/js/appPrototype.js
+++ proJavascript/js/appPrototype.js
@@ -28,17 +28,51 @@
 /* }; */
 
 // (1)>
-function heroProto(name, firstName) {
+function HeroProto({ name, firstName, approveTeam }) {
   //const hero = Object.create(heroProto.prototype);
   this.name = name;
   this.firstName = firstName;
+  this.approveTeam = [];
   //return hero;
 }
 
 //(2)>
-heroProto.prototype.hello = function () {
+HeroProto.prototype.hello = function () {
   console.log(`Hola, yo soy: ${this.name} ${this.firstName}`);
 };
+//New Method>
+HeroProto.prototype.addTeam = function (newTeam) {
+  console.log("this", this);
+  this.approveTeam.push(newTeam);
+};
 
-const mario = new heroProto("Mario", "Bros");
+const mario = new HeroProto({
+  name: "Mario",
+  firstName: "Bros",
+  approveTeam: (approveMoney = []),
+});
 mario.hello();
+mario.addTeam("Manchester");
+console.log(mario);
+console.log(Object.keys(mario));
+console.log(Object.getPrototypeOf(mario));
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+const luisa = {
+  name: "Luisa",
+  age: 30,
+  approveMoney: [],
+
+  // Method
+  addMoney(newMoney) {
+    console.log("this", this);
+    this.approveMoney.push(newMoney);
+  },
+};
+
+//>>
+luisa.addMoney(500);
+console.log(luisa);
+
+console.log(Object.keys(luisa));
+console.log(Object.getPrototypeOf(luisa));

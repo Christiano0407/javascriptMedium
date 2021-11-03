323f92d3cb8530679a110efb7f576c59287b103d
Christiano0407 <placebo0704@gmail.com>
2021-11-03 12:41:42 -0600

    Intersection Observer

--- proJavascript/js/appVideoJs.js
+++ proJavascript/js/appVideoJs.js
@@ -2,7 +2,7 @@ console.group("Platzi Video Media");
 // Const>>>
 // <<== Video Const ==>>
 export const video = document.getElementById(`movie`);
-console.log(movie);
+console.log(video);
 export const playBtn = document.getElementById(`playButton`);
 export const playIconBtn = playBtn.querySelector("i");
 export const muteBtn = document.getElementById(`muteButton`);
@@ -10,6 +10,7 @@ export const volBtnIcon = muteBtn.querySelector("i");
 export const stopBtn = document.getElementById(`stopBtn`);
 export const progressBar = document.querySelector(".progress");
 export const timestamp = document.querySelector(".timestamp");
+
 // <<< == Plugins== Importamos y Ejecutamos ==== >>>
 import { mediaPlayer } from "./videoOne.js";
 
@@ -22,3 +23,20 @@ import { stopVideo } from "./videoOne.js";
 import { setVideoProgress } from "./videoOne.js";
 
 import { updateVideoProgress } from "./videoOne.js";
+// <<<<<<<<<<<<<<<<< blog / Intersection Observer >>>>>>>>>>>>>>>>>>>>>>>
+const imgOne = document.getElementById(`imgOne`);
+
+//> function de cargar Imagen>
+const loadingImage = () => {
+  console.log("Ejecutando");
+};
+
+//> Observador / invocar / parámetros>
+const allObserver = new IntersectionObserver(loadingImage, {
+  root: null,
+  rootMargin: `0px`, //100px, 0, 0, 0,
+  threshold: 0.5, //0.0 / 1.0(todo el elemento dentro),
+});
+
+//> Llamar al Obsevador>
+allObserver.observe(imgOne);
---
+++ proJavascript/js/proIntersectionObserver.js
@@ -0,0 +1,23 @@
+//>
+//export const container = document.getElementById(`container`);
+//export const imgOne = document.getElementById(`imgOne`);
+
+//import { container } from "./appVideoJs";
+//>>>>>>>
+/* import { imgOne } from "./appVideoJs"; */
+/* //> function de cargar Imagen> */
+/* export const loadingImage = () => { */
+/*   console.log("Ejecutando"); */
+/* }; */
+/*  */
+/* //> Observador / invocar / parámetros> */
+/* export const allObserver = new IntersectionObserver(loadingImage, { */
+/*   root: null, */
+/*   rootMargin: 0, //100px, 0, 0, 0, */
+/*   threshold: 0.5, //0.0 / 1.0(todo el elemento dentro), */
+/* }); */
+/*  */
+/* //> Llamar al Obsevador> */
+/* allObserver.observe(imgOne); */
+
+//console.log("Hola");
